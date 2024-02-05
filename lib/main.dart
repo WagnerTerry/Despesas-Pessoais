0ecd355cbc6cdebcd32331527f5957dacea43f36
Wagner Terry <goncalveswagner15@gmail.com>
2024-02-05 13:22:46 -0300

    style: Estilo com Theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,20 @@ import 'dart:math';
 import 'components/transaction_list.dart';
 import 'models/transaction.dart';
 
-main() => runApp(const ExpensesApp());
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({Key? key}) : super(key: key);
+  ExpensesApp({Key? key}) : super(key: key);
+  final ThemeData tema = ThemeData();
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
+      theme: tema.copyWith(
+          colorScheme: tema.colorScheme.copyWith(
+        primary: Colors.purple,
+        secondary: Colors.amber,
+      )),
       debugShowCheckedModeBanner: false,
     );
   }
